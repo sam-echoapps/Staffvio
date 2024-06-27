846c09a173c6a8c67d519c2729b4d8407b460012
sam-echoapps <sam@echoapps360.com>
2024-06-27 19:27:15 +0530

    Privacy Policy Commit

--- src/js/appController.js
+++ src/js/appController.js
@@ -61,7 +61,8 @@ define([ 'ojs/ojoffcanvas' , 'knockout', 'ojs/ojmodule-element-utils', 'ojs/ojre
           { path: 'myworkHistory', detail : {label: 'My Work History',iconClass: 'oj-navigationlist-item-icon fa fa-edit'} },
           { path: 'myCalender', detail : {label: 'My Calendar',iconClass: 'oj-navigationlist-item-icon fa fa-calendar'} },
           { path: 'timeSheetStaff', detail : {label: 'Time Sheet',iconClass: 'oj-navigationlist-item-icon fa fa-clock'}},
-          { path: 'help', detail : {label: 'Help',iconClass: 'oj-navigationlist-item-icon fa fa-question-circle'} }
+          { path: 'help', detail : {label: 'Help',iconClass: 'oj-navigationlist-item-icon fa fa-question-circle'} },
+          { path: 'privacyPolicy', detail : {label :'Privacy policy',iconClass: 'oj-navigationlist-item-icon fa fa-id-card'} },
         ];  
       }else if(sessionStorage.getItem('userRole')=='Accounts'){
         var navData = [
@@ -139,6 +140,7 @@ define([ 'ojs/ojoffcanvas' , 'knockout', 'ojs/ojmodule-element-utils', 'ojs/ojre
           { path: 'dashboardAdmin', detail : {label: 'Dashboard',iconClass: 'oj-navigationlist-item-icon fa fa-home'} },
           { path: 'dashboardChart', detail : {label: 'Dashboard Chart',iconClass: 'oj-navigationlist-item-icon fa fa-home'} },
           { path: 'dashboardClient', detail : {label: 'Dashboard',iconClass: 'oj-navigationlist-item-icon fa fa-home'} },
+          { path: 'privacyPolicy', detail : {label :'Privacy policy',iconClass: 'oj-navigationlist-item-icon fa fa-id-card'} },
 /*           { path: 'postcode', detail : {label: 'Postcode',iconClass: 'oj-navigationlist-item-icon fa fa-calendar'} }
  */
         ];
